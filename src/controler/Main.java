6b5bb0f89240f62c9c368b4c30f64876257a39ec
rathahausong <simrathahausongrupp@gmail.com>
2016-06-06 14:09:48 +0700

    connection

--- src/controler/Main.java
+++ src/controler/Main.java
@@ -1,14 +1,18 @@
 package controler;
-
-import java.sql.Connection;
-
-import JDBC.IConnection;
-
+import dao.UserDao;
+import dto.User;
 public class Main {
 	public static void main(String[] args) {
-		
+		User u = new User();
+		u.setId(1);
+		u.setUsername("sim ratha");
+		u.setPassword("123456");
 		try {
-			IConnection.CONNECTION();
+			if(new UserDao().insertRow(u)){
+				System.out.println("insert is complet !");
+			}else{
+				System.out.println("can not insert !");
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -1,17 +1,40 @@
 package dao;
 
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
-import JDBC.IDatabase;
+import dto.User;
+import jdbc.IConnection;
+import jdbc.IDatabase;
 
 public class UserDao implements IDatabase {
-	
-	
-	
 	@Override
 	public boolean insertRow(Object obj) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		User user = (User)obj;
+		Connection con = null;
+		@SuppressWarnings("unused")
+		Statement st = null;
+		PreparedStatement pst = null;
+		try{
+			con = IConnection.CONNECTION();
+			String query="insert into tbuser values(?,?,?)";
+			pst = con.prepareStatement(query);
+			pst.setInt(1,user.getId());
+			pst.setString(2,user.getUsername());
+			pst.setString(3, user.getPassword());
+			pst.executeUpdate();
+			
+		}catch(Exception ex){
+			return false;
+		}finally{
+			if(con!=null){
+				pst.close();
+				con.close();
+			}
+		}
+		return true;
 	}
 
 	@Override
